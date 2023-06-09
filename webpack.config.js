1dab20f5ad91775ea8dcbbd5e37c3d1682005d93
Ruthmy <Ruryemo.exe@gmail.com>
2023-06-09 15:11:35 -0600

    Trying to fix errors with Webhint

--- webpack.config.js
+++ webpack.config.js
@@ -35,6 +35,16 @@ module.exports = {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
         type: 'asset/resource',
       },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
     ],
   },
 };
